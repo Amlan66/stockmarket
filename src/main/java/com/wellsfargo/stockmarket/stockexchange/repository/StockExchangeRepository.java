fb297c91b39edc6681dcd151ab2c747bac8c9348
shubhamg763 <shubhamg763@gmail.com>
2020-08-30 15:29:46 +0530

    Add files via upload

---
+++ src/main/java/com/wellsfargo/stockmarket/stockexchange/repository/StockExchangeRepository.java
@@ -0,0 +1,9 @@
+package com.wellsfargo.stockmarket.stockexchange.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.wellsfargo.stockmarket.stockexchange.entity.StockExchange;
+
+public interface StockExchangeRepository extends JpaRepository<StockExchange, Integer> {
+
+}
